43426fa978d143496d31e6fc16c5eb4e0b987313
Texas R3X <texasr3xphone@gmail.com>
2026-04-26 17:00:55 -0600

    Update `dev.sh` to handle different compile options
    - Add `build-cpp` option to build CPP & HPP files
    - Add `build-cmake` option to build CMake, CPP, & HPP files
    - Add `build-conan` option to build/install Conan, CMake, CPP, & HPP files

--- dev.sh
+++ dev.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
+
+# Ensure that if an error occures, the script stops
 set -e
+
+# Declare helper functions
+install_conan_deps() {
+    # Install the Conan dependencies
+    conan install .. --build=missing
+}
+config_cmake_and_conan() {
+    # Configure CMake the Conan toolchain
+    cmake .. -DCMAKE_TOOLCHAIN_FILE=Release/generators/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
+}
+build_executable() {
+    # Build the unit test executable
+    cmake --build .
+}
 
 # Force this script to run in the right directory
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -12,17 +28,23 @@ mkdir -p build
 # Move into the `build/` folder
 cd build
 
-# Install the Conan dependencies
-conan install .. --build=missing
+# Compile/configure the things that need to be compiled/configured
+if [[ "$1" == "build-conan" ]]; then
+    install_conan_deps
+    config_cmake_and_conan
+    build_executable
+elif [[ "$1" == "build-cmake" ]]; then
+    config_cmake_and_conan
+    build_executable
+elif [[ -z "$1" || "$1" == "build-cpp" ]]; then
+    build_executable
+else
+    echo "Error: Unknown arguement for dev.sh: $1"
+    exit 1
+fi
 
-# Configure CMake the Conan toolchain
-cmake .. -DCMAKE_TOOLCHAIN_FILE=Release/generators/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
-
-# Build the unit test executable
-cmake --build .
-
-# Move to the root directory
-cd ..
+# Move to the `./build/tests/` directory
+cd tests
 
 # Run the unit test executable
-./build/tests/test_sparrow_math
+./test_sparrow_math

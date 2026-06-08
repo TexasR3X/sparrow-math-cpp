b608eabad39eae90758b5526009553266d17e0ff
Texas R3X <texasr3xphone@gmail.com>
2026-06-08 17:00:01 -0600

    Replace `THROW_INVALID_NAME_ERROR` with `ThrowInvalidNameError()` in `TrimSymbolName()`
    - Rename `IterationPhase::FoundBackslashSignButNoLetters` to `IterationPhase::FoundBackslashButNoLetters`
    - Make it so  '%' can't be used instead of '\\'

--- src/internal/str_utils.cpp
+++ src/internal/str_utils.cpp
@@ -5,47 +5,46 @@
 
 namespace sparrow_math::internal {
     std::string TrimSymbolName(const std::string& name) {
-        #define THROW_INVALID_NAME_ERROR throw std::runtime_error(std::format("Invalid symbol name: \"{}\"", name));
-
         enum class IterationPhase {
             FoundNothingOrLeadingWhitespace,
-            FoundBackslashSignButNoLetters,
+            FoundBackslashButNoLetters,
             FoundLetters,
             FoundTrailingWhitespace
+        };
+
+        auto ThrowInvalidNameError = [&name] {
+            throw std::runtime_error(std::format("Invalid symbol name: \"{}\"", name));
         };
 
         std::string trimmedName;
         IterationPhase iterationPhase = IterationPhase::FoundNothingOrLeadingWhitespace;
 
-        for (auto ch : name) {
+        for (const auto ch : name) {
             switch (iterationPhase) {
-                // Found nothing or found leading whitespace
                 case IterationPhase::FoundNothingOrLeadingWhitespace:
                     if (IsCharWhitespace(ch)) {
                         // Ignore the whitespace character
                     }
-                    else if (ch == '%' || ch == '\\') {
-                        iterationPhase = IterationPhase::FoundBackslashSignButNoLetters;
+                    else if (ch == '\\') {
+                        iterationPhase = IterationPhase::FoundBackslashButNoLetters;
                     }
                     else if (IsCharAlphabetical(ch)) {
                         trimmedName += ch;
                         iterationPhase = IterationPhase::FoundLetters;
                     }
                     else {
-                        THROW_INVALID_NAME_ERROR
+                        ThrowInvalidNameError();
                     }
                     break;
-                // Found percent sign (or backslash) but no letters yet
-                case IterationPhase::FoundBackslashSignButNoLetters:
+                case IterationPhase::FoundBackslashButNoLetters:
                     if (IsCharAlphabetical(ch)) {
                         trimmedName += ch;
                         iterationPhase = IterationPhase::FoundLetters;
                     }
                     else {
-                        THROW_INVALID_NAME_ERROR
+                        ThrowInvalidNameError();
                     }
                     break;
-                // Found letters
                 case IterationPhase::FoundLetters:
                     if (IsCharWhitespace(ch)) {
                         iterationPhase = IterationPhase::FoundTrailingWhitespace;
@@ -54,22 +53,16 @@ namespace sparrow_math::internal {
                         trimmedName += ch;
                     }
                     else {
-                        THROW_INVALID_NAME_ERROR
+                        ThrowInvalidNameError();
                     }
                     break;
-                // Found trailing whitespace
                 case IterationPhase::FoundTrailingWhitespace:
                     if (!IsCharWhitespace(ch)) {
-                        THROW_INVALID_NAME_ERROR
+                        ThrowInvalidNameError();
                     }
-                    break;
-                default:
-                    throw std::runtime_error("Line that shouldn't be reached");
             }
         }
 
         return trimmedName;
-
-        #undef THROW_INVALID_NAME_ERROR
     }
 }
